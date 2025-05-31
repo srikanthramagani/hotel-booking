219365a4fa46189c312502884bee860aa6109b79
srikanthramagani <168701160+srikanthramagani@users.noreply.github.com>
2025-05-31 15:26:13 +0530

    Update clerkWebhooks.js

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -23,7 +23,7 @@ const clerkWebhooks=async(req,res)=>{
         const userData={
             _id:data.id,
             email:data.email_addresses[0].email_address,
-            username: data.first_name +""+data.last_name,
+            username: data.first_name +" "+data.last_name,
             image: data.image_url,
 
         }
@@ -49,7 +49,7 @@ const clerkWebhooks=async(req,res)=>{
         res.json({success:true, message:"webhook Recieved"})
 
     }catch(error){
-        console.log(error.message());
+        console.log(error.message);
         res.json({success:false, message: error.message});
 
     }
