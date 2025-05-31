9f6768346a10b3caf646bc470547267c32c08bdd
srikanthramagani <168701160+srikanthramagani@users.noreply.github.com>
2025-05-31 13:54:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(clerkMiddleware())
 
 //API to listen to Clerk Webhooks
 
-app.use("/api/clerk",clerkWebhooks);
+app.post("/api/clerk",clerkWebhooks);
 
 app.get('/',(req,res)=> res.send("API is working"))
 
